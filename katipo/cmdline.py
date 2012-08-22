84e642fbbb9e2c4b710e2b7784d0b77652c59ec0
Jonathan J Hunt <hunt@braincorporation.com>
2012-08-21 17:36:44 -0700

    Working arrangment of commands.

--- katipo/cmdline.py
+++ katipo/cmdline.py
@@ -20,10 +20,78 @@
 
 """The command line interface to katipo. Run using script in root folder."""
 
+import sys
+import argparse
+import logging
+import abc
+
+
+def register_cmd(future_class_name, future_class_parents, future_class_attr):
+		print future_class_name
+		return type(future_class_name, future_class_parents, future_class_attr)
+
+
+class Command(object):
+	@abc.abstractmethod
+	def construct_parser(self, parser):
+		pass
+
+	@abc.abstractmethod
+	def exec_cmd(self, args):
+		pass
+
+	@property
+	def command_name(self):
+		return type(self).__name__.split('_')[1]
+
+	@classmethod
+	def get_commands(cls):
+		if not hasattr(cls, '_commands'):
+			cls._commands = [cmd() for cmd in Command.__subclasses__()]
+		return cls._commands
+
+
+class Command_clone(Command):
+	'Clone working copy from assembly description'
+	def construct_parser(self, parser):
+		parser.add_argument('assemblyrepo', type=str)
+		parser.add_argument('assemblyfile', type=str)
+
+	def exec_cmd(self, args):
+		print 'clone %s' % str(args)
+
+
+class Command_about(Command):
+	'Learn more about katipo'
+	def construct_parser(self, parser):
+		pass
+
+	def exec_cmd(self, args):
+		print 'about'
+
+
+def build_arg_parser():
+	toplevel_parser = argparse.ArgumentParser(description=
+											'katipo: deal with multiple git repos')
+	subparsers = toplevel_parser.add_subparsers(help='sub-command help')
+	for cmd in Command.get_commands():
+		logging.info('Adding command %s', cmd.command_name)
+		cmd_parser = subparsers.add_parser(cmd.command_name, help=cmd.__doc__)
+		cmd_parser.set_defaults(cmd=cmd)
+		cmd.construct_parser(cmd_parser)
+	return toplevel_parser
+
+
+def run_args(args):
+	"""Execute the arguments passed."""
+	logging.info('Args %s' % str(args))
+	parser = build_arg_parser()
+	input = parser.parse_args(args)
+
 
 def main():
 	"""First function called when invoked from the command line."""
-	print 'Hello'
+	run_args(sys.argv[1:])
 
 if __name__ == '__main__':
 	main()
