9a100c50d1cbec44b372c3b5e1bc4b6f5dcace44
Jonathan J Hunt <hunt@braincorporation.com>
2013-05-29 11:26:04 -0700

    Updated licenses.

--- katipo/test_reposetup.py
+++ katipo/test_reposetup.py
@@ -1,22 +1,9 @@
 # ===========================================================================
 #
-#  COPYRIGHT 2012 Brain Corporation.
-#  All rights reserved. Brain Corporation proprietary and confidential.
-#
-#  The party receiving this software directly from Brain Corporation
-#  (the "Recipient" ) may use this software and make copies thereof as
-#  reasonably necessary solely for the purposes set forth in the agreement
-#  between the Recipient and Brain Corporation ( the "Agreement" ). The
-#  software may be used in source code form
-#  solely by the Recipient's employees. The Recipient shall have no right to
-#  sublicense, assign, transfer or otherwise provide the source code to any
-#  third party. Subject to the terms and conditions set forth in the Agreement,
-#  this software, in binary form only, may be distributed by the Recipient to
-#  its customers. Brain Corporation retains all ownership rights in and to
-#  the software.
-#
-#  This notice shall supercede any other notices contained within the software.
+#  COPYRIGHT 2012-2013 Brain Corporation.
+#  License under MIT license (see LICENSE file)
 # =============================================================================
+
 """Helper class for initializing a katipo repo based on a variant of an
 assemblyfile (also contains the repo contents for ease of testing)."""
 
